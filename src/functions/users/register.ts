a9d2a623461587ce901e8c607e85be4cc5b9fb5e
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-23 18:54:06 -0500

    create new function for register user with type

---
+++ src/functions/users/register.ts
@@ -0,0 +1,21 @@
+import {APIGatewayProxyHandler} from "aws-lambda";
+import responseObject from "../../utils/Response";
+import UsersAuthService from "../../services/users.auth.service";
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+    console.log(`HANDLER: Starting ${context.functionName}...`);
+
+    if (typeof event.body === "string"){
+        const {name, email, password, type} = JSON.parse(event.body);
+
+        if (name.trim() === "")
+            return responseObject(400, { message: "Name is required" });
+        else if (email.trim() === "")
+            return responseObject(400, { message: "Email is required" });
+        else if (password.trim() === "")
+            return responseObject(400, { message: "Password is required" });
+
+        return await UsersAuthService.registerUser({name, email, password, type});
+    }
+    return responseObject(400, {message: "Body is required"});
+};
