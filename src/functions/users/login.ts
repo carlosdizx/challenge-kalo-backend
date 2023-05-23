d7c692452061e81da54ba123c273ff975ec3eaeb
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-23 18:54:51 -0500

    create new function for login user and return token

---
+++ src/functions/users/login.ts
@@ -0,0 +1,19 @@
+import {APIGatewayProxyHandler} from "aws-lambda";
+import responseObject from "../../utils/Response";
+import UsersAuthService from "../../services/users.auth.service";
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+    console.log(`HANDLER: Starting ${context.functionName}...`);
+
+    if (typeof event.body === "string"){
+        const {name, email, password} = JSON.parse(event.body);
+
+       if (email.trim() === "")
+            return responseObject(400, { message: "Email is required" });
+        else if (password.trim() === "")
+            return responseObject(400, { message: "Password is required" });
+
+        return await UsersAuthService.loginUser({email, password});
+    }
+    return responseObject(400, {message: "Body is required"});
+};
