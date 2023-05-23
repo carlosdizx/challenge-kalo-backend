b27c721287bd0abd102b3f1436adf6aa0cfc4986
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-23 18:17:57 -0500

    create new util for upload file from buffer

---
+++ src/utils/S3Config.ts
@@ -0,0 +1,22 @@
+import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+const Bucket = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const s3Client = new S3Client({ region });
+
+export const upload = async ({Key, Body}: {Key: string, Body: Buffer }) =>
+{
+    console.time("Upload File");
+    const uploadCommand = new PutObjectCommand({
+        Bucket,
+        Key,
+        Body
+    });
+    
+    try {
+        const response = await s3Client.send(uploadCommand);
+        console.timeEnd("Upload File");
+        return response;
+    }catch (e) {
+        throw e;
+    }
+}
