5d3c786299060c8aa81f3a1aedda89db734f6071
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-23 17:02:53 -0500

    created file with configure for connect to database

---
+++ src/utils/DatabaseConnection.ts
@@ -0,0 +1,28 @@
+import { DataSource } from "typeorm";
+import dotenv = require("dotenv");
+import User from "../entities/User.entity";
+dotenv.config();
+
+const dataSource =  new DataSource({
+    type: "postgres",
+    host: process.env.DB_POSTGRES_HOST,
+    port: parseInt(`${process.env.DB_POSTGRES_PORT}`),
+    username: process.env.DB_POSTGRES_USER,
+    password: process.env.DB_POSTGRES_PASSWORD,
+    database: process.env.DB_POSTGRES_DATABASE,
+    synchronize: true,
+    entities: [
+        User,
+    ],
+})
+
+const getConnect = async () => {
+    if(!dataSource.isInitialized) {
+        console.log("Trying to connect to the database");
+        await dataSource.initialize();
+    }else console.log("recycled connection");
+    console.log("Successfully connected!");
+    return dataSource;
+}
+
+export default getConnect;
