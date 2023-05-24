97eff97da02d119a21b2ab1c5a818764d66a5ffe
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-24 03:28:38 -0500

    install http-multipart-body-parser from middy and usage for capture fields of body

--- src/functions/articles/create.ts
+++ src/functions/articles/create.ts
@@ -3,19 +3,26 @@ import responseObject from "../../utils/Response";
 import middy from "@middy/core";
 import hasTokenValid from "../../middleware/hasTokenValid";
 import {TypesUser} from "../../Enums/typesUser";
+import multipartBodyParser from '@middy/http-multipart-body-parser';
 
-const originalHandler: APIGatewayProxyHandler = async (event, context) => {
+const originalHandler = async (event, context) => {
     console.log(`HANDLER: Starting ${context.functionName}...`);
 
-    if (typeof event.body === "string"){
-        console.log("--------------------------------");
-        console.log(event.body);
-        console.log("--------------------------------");
-        return responseObject(400, {message: "Body is required"});
+    if (event.body) {
+        const {title, body, image} = event.body;
+
+        console.log('title:', title);
+        console.log('body:', body);
+        console.log('image:', image);
+        console.log('typeof image:', typeof image);
+
+        return responseObject(200, { message: 'Form data processed successfully' });
     }
-    return responseObject(400, {message: "Body is required"});
+
+    return responseObject(400, { message: 'Form data is required' });
 };
 
 export const handler = middy()
+    .use(multipartBodyParser())
     .use(hasTokenValid([TypesUser.USER, TypesUser.ADMIN]))
     .handler(originalHandler);
