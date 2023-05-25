53f5475bd1a2178aebb1bee899ce1c52759173d6
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-25 16:18:15 -0500

    capture name and file content of body

--- src/functions/articles/create.ts
+++ src/functions/articles/create.ts
@@ -2,28 +2,18 @@ import responseObject from "../../utils/Response";
 import middy from "@middy/core";
 import hasTokenValid from "../../middleware/hasTokenValid";
 import {TypesUser} from "../../Enums/typesUser";
-import multipartBodyParser from "@middy/http-multipart-body-parser";
 import ArticlesCrudService from "../../services/articles.crud.service";
 const allowedMimes = ['image/jpeg', 'image/png', 'image/jpg'];
 
 const originalHandler = async (event, context) => {
     console.log(`HANDLER: Starting ${context.functionName}...`);
-
-    if (event.body) {
-        const {title, body, image} = event.body;
-        if (image) {
-            const { filename, mimetype, encoding, truncated, content } = image;
-            if(!allowedMimes.includes(mimetype))
-                return responseObject(400,{message: "File is not image!"});
-
-            const result = await ArticlesCrudService.createArticle({title,body, image});
-
-            return responseObject(200, { message: 'Form data processed successfully', result });
-        } else
-            return responseObject(400, { message: 'Image data is required' });
+    if(event.body){
+        const data = event.body.split("\r\n");
+        const fileName = data[1].split(";")[2].split("=")[1].replace(/^"|"$/g, '').trim();
+        const fileContent = data[4].trim();
+        return responseObject(200, {message: 'Form data is required', fileName,fileContent});
     }
-
-    return responseObject(400, { message: 'Form data is required' });
+    return responseObject(200, {message: 'Form data is required'});
 };
 
 export const handler = middy()
