1d1b3cfe11adf2f13ff19a3851f57c235bc2e691
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-25 16:55:34 -0500

    create new method for create article

--- src/dao/Article.dao.ts
+++ src/dao/Article.dao.ts
@@ -1,12 +1,13 @@
 import getConnect from "../utils/DatabaseConnection";
 import Article from "../entities/Article.entity";
+import User from "../entities/User.entity";
 
 export default class ArticleDao {
 
-    public static create = async (title: string,  body: string, image: any) => {
+    public static create = async (title: string,  body: string, user: User) => {
         const datasource = await getConnect();
         const repository = datasource.getRepository(Article);
-        const article = repository.create({ title, body, image });
+        const article = repository.create({ title, body });
         return await repository.save(article);
     }
 }
--- src/services/articles.crud.service.ts
+++ src/services/articles.crud.service.ts
@@ -1,7 +1,22 @@
 import {upload} from "../utils/S3Config";
 import responseObject from "../utils/Response";
+import ArticleDao from "../dao/Article.dao";
+import UserDao from "../dao/User.dao";
 
 export default class ArticlesCrudService {
+
+    public static createArticle = async ({title, body}:{title: string, body: string}, userId: string) =>{
+        const user = await UserDao.findById(userId);
+        if(!user)
+            return responseObject(409, {message: "You do not have permission!"});
+        try {
+            const article = ArticleDao.create(title, body, user);
+            return responseObject(200, {message: "Article created"});
+        }
+        catch (e) {
+            return responseObject(500, {message: "Article creation error,", e})
+        }
+    }
     public static uploadFileByArticle = async (
         {image, name, type}:{image: Buffer, name: string, type: string}, articleId: string, userId: string
     ) => {
