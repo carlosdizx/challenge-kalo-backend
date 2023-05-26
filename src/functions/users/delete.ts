30dd0f316e3cd3ed853f932fc368e8a540cb4317
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-26 15:12:24 -0500

    create new function for delete user

---
+++ src/functions/users/delete.ts
@@ -0,0 +1,14 @@
+import middy from "@middy/core";
+import hasTokenValid from "../../middleware/hasTokenValid";
+import {TypesUser} from "../../Enums/typesUser";
+import UserCrudService from "../../services/user.crud.service";
+
+const originalHandler = async (event, context) => {
+    console.log(`HANDLER: Starting ${context.functionName}...`);
+    const {id} = event.pathParameters;
+    return await UserCrudService.deleteById(id);
+};
+
+export const handler = middy()
+    .use(hasTokenValid([TypesUser.ADMIN]))
+    .handler(originalHandler);
