cbac1486332522c2f3c09a42841769eedcba7385
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-26 14:53:20 -0500

    add validation for accept type user only if is owner the data

--- src/functions/users/find.ts
+++ src/functions/users/find.ts
@@ -1,14 +1,21 @@
 import {APIGatewayProxyHandler} from "aws-lambda";
 import middy from "@middy/core";
 import hasTokenValid from "../../middleware/hasTokenValid";
-import {TypesUser} from "../../Enums/typesUser";
+import {parseTypeUser, TypesUser} from "../../Enums/typesUser";
 import UserCrudService from "../../services/user.crud.service";
+import responseObject from "../../utils/Response";
 
 const originalHandler: APIGatewayProxyHandler = async (event, context) => {
     console.log(`HANDLER: Starting ${context.functionName}...`);
     const {id} = event.pathParameters;
-    return UserCrudService.findUserById(id);
+    const userId = event.requestContext.authorizer.jwt.claims.id;
+    const response = await UserCrudService.findUserById(id);
+    const user = JSON.parse(response.body);
+    const typeUserFound = parseTypeUser(user.type);
+    if(typeUserFound === null || (typeUserFound === TypesUser.USER && id !== userId))
+        return responseObject(403, { message: "You have not permissions" });
+    return response;
 };
 
 export const handler = middy(originalHandler)
-    .use(hasTokenValid([TypesUser.ADMIN]));
+    .use(hasTokenValid([TypesUser.ADMIN, TypesUser.USER]));
