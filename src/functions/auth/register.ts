14652d770e72b2f16c9b554bd4a289c3f7389409
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-29 00:07:11 -0500

    change logic in lambda pre format data

--- src/functions/auth/register.ts
+++ src/functions/auth/register.ts
@@ -1,21 +1,26 @@
-import {APIGatewayProxyHandler} from "aws-lambda";
 import responseObject from "../../utils/Response";
 import UsersAuthService from "../../services/users.auth.service";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
 
-export const handler: APIGatewayProxyHandler = async (event, context) => {
+export const originalHandler = async (event, context) => {
     console.log(`HANDLER: Starting ${context.functionName}...`);
 
-    if (typeof event.body === "string"){
-        const {name, email, password} = JSON.parse(event.body);
+    if(!event.body)
+        return responseObject(400, {message: "Body is required"});
 
-        if (name.trim() === "")
-            return responseObject(400, { message: "Name is required" });
-        else if (email.trim() === "")
-            return responseObject(400, { message: "Email is required" });
-        else if (password.trim() === "")
-            return responseObject(400, { message: "Password is required" });
+    const {name, email, password} = event.body;
 
-        return await UsersAuthService.registerUser({name, email, password});
-    }
-    return responseObject(400, {message: "Body is required"});
+    if (name.trim() === "")
+        return responseObject(400, { message: "Name is required" });
+    else if (email.trim() === "")
+        return responseObject(400, { message: "Email is required" });
+    else if (password.trim() === "")
+        return responseObject(400, { message: "Password is required" });
+
+    return await UsersAuthService.registerUser({name, email, password});
 };
+
+export const handler = middy()
+    .use(httpJsonBodyParser())
+    .handler(originalHandler);
