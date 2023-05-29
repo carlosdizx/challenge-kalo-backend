407fc12178a3e4ce4c26a60053ac0fde84376077
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-29 18:54:44 -0500

    add property, headers for response in lambda function

--- src/utils/Response.ts
+++ src/utils/Response.ts
@@ -1,8 +1,11 @@
 interface Response {
     statusCode: number;
     body: string;
+    headers: object;
 }
 
-const responseObject = (code: number, body: any) : Response => ({statusCode: code, body: JSON.stringify(body)});
+const responseObject = (code: number, body: any, headers: object = {
+    'Content-Type':'application/json'
+}) : Response => ({statusCode: code, body: JSON.stringify(body), headers});
 
 export default responseObject;
