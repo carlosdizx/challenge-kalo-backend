e47ff0a5ef90de3e7bd9c63c0ce596e63b176933
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-29 22:15:09 -0500

    usage method for get id of token in articles/create

--- src/functions/articles/create.ts
+++ src/functions/articles/create.ts
@@ -1,19 +1,23 @@
-import {APIGatewayProxyHandler} from "aws-lambda";
 import responseObject from "../../utils/Response";
 import middy from "@middy/core";
 import hasTokenValid from "../../middleware/hasTokenValid";
 import {TypesUser} from "../../enums/typesUser";
 import ArticlesCrudService from "../../services/articles.crud.service";
-const originalHandler: APIGatewayProxyHandler  = async (event, context) => {
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import {getUserId} from "../../utils/AuthUtils";
+const originalHandler  = async (event, context) => {
     console.log(`HANDLER: Starting ${context.functionName}...`);
-    if(typeof event.body === "string") {
-        const userId = event.requestContext.authorizer.jwt.claims.id;
-        const {title, body} = JSON.parse(event.body);
-        return await ArticlesCrudService.createArticle({title, body}, userId);
-    }
-    return responseObject(400, {message: 'Form data is required'});
+    if(!event.body)
+        return responseObject(400, {message: 'Form data is required'});
+    const token = event.headers.authorization.split(" ")[1];
+    if(!token)
+        return responseObject(401, {message: 'Check your token!'});
+    const userId = getUserId(token)
+    const {title, body} = event.body;
+    return await ArticlesCrudService.createArticle({title, body}, userId);
 };
 
 export const handler = middy()
     .use(hasTokenValid([TypesUser.USER]))
+    .use(httpJsonBodyParser())
     .handler(originalHandler);
