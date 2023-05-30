457f14e47564dbce337b60f0406a0c95e245493f
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-30 18:25:40 -0500

    update logic for update user

--- src/functions/users/update.ts
+++ src/functions/users/update.ts
@@ -4,26 +4,36 @@ import {parseTypeUser, TypesUser} from "../../enums/typesUser";
 import httpJsonBodyParser from '@middy/http-json-body-parser';
 import UserCrudService from "../../services/user.crud.service";
 import responseObject from "../../utils/Response";
-import {getUserId} from "../../utils/AuthUtils";
+import {getUser, getUserId} from "../../utils/AuthUtils";
 
 const originalHandler = async (event, context) => {
     console.log(`HANDLER: Starting ${context.functionName}...`);
-    const {id} = event.pathParameters;
-    const token = event.headers.authorization.split(" ")[1];
-    const userId = getUserId(token);
-    const {body} = await UserCrudService.findUserById(userId);
-    const user = JSON.parse(body);
-    const {name, email, password} = event.body;
-    const typeUserFound = parseTypeUser(user.type);
 
+    const token = event.headers.authorization.split(" ")[1];
+    const requestingUser = getUser(token);
+    if(!requestingUser)
+        return responseObject(409, {message: 'JWT is invalid'});
+
+    const {id} = event.pathParameters;
+    const response = await UserCrudService.findUserById(id);
+    const typeUserReq = parseTypeUser(requestingUser.type);
+
+    if (typeUserReq !== TypesUser.ADMIN){
+        if(response.statusCode !== 200)
+            return responseObject(404, { message: "User not found or id is invalid" });
+        const userFound = JSON.parse(response.body);
+        if(requestingUser.id !== userFound.id)
+            return responseObject(403, { message: "You do not have permission" });
+    }
+
+    const {name, email, password} = event.body;
     if (name && name.trim() === "")
         return responseObject(400, { message: "Name is required" });
     else if (email && email.trim() === "")
         return responseObject(400, { message: "Email is required" });
     else if (password && password.trim() === "")
         return responseObject(400, { message: "Password is required" });
-    else if(typeUserFound === null || (typeUserFound === TypesUser.USER && id !== userId))
-        return responseObject(403, { message: "You have not permissions" });
+
     return await UserCrudService.update({name, email, password}, id);
 };
 
