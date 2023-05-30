3134f38f9171744d6f63b87eff6a9e73a4cfbb38
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-29 21:18:21 -0500

    configure s3 for object in bucket will are public

--- src/utils/Response.ts
+++ src/utils/Response.ts
@@ -1,11 +1,14 @@
-interface Response {
-    statusCode: number;
-    body: string;
-    headers: object;
+import {Response} from 'lambda-proxy-utils';
+
+const parser = (statusCode: number, data?: any, headers: object = {
+    "Content-Type":"application/json"
+}) => {
+    data = data || {};
+    return new Response({statusCode, cors: true, headers}).send(JSON.stringify(data));
 }
 
 const responseObject = (code: number, body: any, headers: object = {
     'Content-Type':'application/json'
-}) : Response => ({statusCode: code, body: JSON.stringify(body), headers});
+}) : Response => parser(code, body, headers);
 
 export default responseObject;
