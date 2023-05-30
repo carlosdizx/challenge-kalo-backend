1c5cbf5173aa70f0f99333de6430d5a8f51bf85b
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-30 00:36:28 -0500

    add validation for upload image

--- src/functions/articles/upload-image.ts
+++ src/functions/articles/upload-image.ts
@@ -1,4 +1,3 @@
-import {APIGatewayProxyHandler} from "aws-lambda";
 import responseObject from "../../utils/Response";
 import middy from "@middy/core";
 import hasTokenValid from "../../middleware/hasTokenValid";
@@ -7,7 +6,7 @@ import ArticlesCrudService from "../../services/articles.crud.service";
 import {getUserId} from "../../utils/AuthUtils";
 const allowedMimes = ['image/jpeg', 'image/png', 'image/jpg'];
 
-const originalHandler:APIGatewayProxyHandler  = async (event, context) => {
+const originalHandler  = async (event, context) => {
     console.log(`HANDLER: Starting ${context.functionName}...`);
     if(event.body){
         const token = event.headers.authorization.split(" ")[1];
@@ -20,12 +19,10 @@ const originalHandler:APIGatewayProxyHandler  = async (event, context) => {
         const fileContType = data[2].split(":")[1].trim();
         if(!allowedMimes.includes(fileContType))
             return responseObject(400, {message: "File is not a image!"});
-        console.error("******************");
-        console.log(data[0]);
-        console.log(data[4]);
-        console.error("******************");
         const fileName = data[1].split(";")[2].split("=")[1].replace(/^"|"$/g, '').trim();
-        const fileContent = data[4];
+        let fileContent = data[4];
+        if(data.length === 12)
+            fileContent = fileContent.concat(data[5], data[6], data[7], data[8], data[9], data[10], data[11]);
         const buffer = Buffer.from(fileContent, 'binary');
         return await ArticlesCrudService.uploadFileByArticle({image: buffer, name: fileName, type: fileContType}, articleId, userId);
     }
